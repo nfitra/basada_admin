2b430bf03d9e2569aca690e2a5d4a1170c2dab33
Aziz Kandias <52338619+zeekands@users.noreply.github.com>
2022-11-12 08:00:19 +0700

    add fcm and google services

--- lib/app/modules/home/providers/list_penjemputan_providers.dart
+++ lib/app/modules/home/providers/list_penjemputan_providers.dart
@@ -1,8 +1,11 @@
 import 'package:basada_admin/app/modules/home/models/list_penjemputan.dart';
 import 'package:basada_admin/app/routes/app_pages.dart';
 import 'package:get/get.dart';
+import 'package:dio/dio.dart' as dio;
 
 class ListPenjemputanProvider extends GetConnect {
+  final dio.Dio _dio = dio.Dio();
+
   Future<List<ListPenjemputan>> getListPenjemputan(String bearer) async {
     final headers = {
       'token': bearer,
@@ -16,6 +19,26 @@ class ListPenjemputanProvider extends GetConnect {
     } else {
       final list = response.body as List;
       return list.map((e) => ListPenjemputan.fromJson(e)).toList();
+    }
+  }
+
+  Future<String> addDevice(String token, String registrationId) async {
+    final formData = dio.FormData.fromMap({
+      'registration_id': registrationId,
+    });
+    final response = await _dio.post(
+      '${Routes.BASE_URL}/api/device',
+      data: formData,
+      options: dio.Options(
+        headers: {
+          'token': token,
+        },
+      ),
+    );
+    if (response.statusCode == 200) {
+      return 'success';
+    } else {
+      return Future.error("Gagal menambahkan device");
     }
   }
 }
